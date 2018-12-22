bd1aad4b91bdd7a174eb1ce44849c0774556e31f
MX <4masmix1@gmail.com>
2018-12-22 02:23:14 +0100

    Cleanup

--- src/main/java/bot/modules/gabe_modules/query/TorrentTest.java
+++ src/main/java/bot/modules/gabe_modules/query/TorrentTest.java
@@ -13,21 +13,18 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TorrentTest extends ModuleBase {
-    private static final String X1337_URL = "1337x.to/search/";
-//    private static final String X1337_URL = "rarbg.to/torrents.php?search=";
-    private static final String POSTFIX = "/1/";
-
-    private static final String TORRENTZ_URL = "https://torrentz2.eu/search?f=";
-
-    private final String SEARCH_REGEX = Utils.TO_REGEX("torrent (.*)");
-    private final Bitly bitly;
+    private final String TORRENT_REGEX = Utils.TO_REGEX("torrent (.*)");
+    private final String T_REGEX = Utils.TO_REGEX("t (.*)");
 
     private static final String BITLY_ACCESS_TOKEN = "ccbb8945fa671a57a48645c181466d9ad5619749";
 
+    private static final String X1337_URL = "1337x.to/search/";
+    private static final String X1337_POSTFIX = "/1/";
+
+    private static final String TORRENTZ_URL = "https://torrentz2.eu/search?f=";
+
     public TorrentTest(Chatbot chatbot) {
         super(chatbot);
-
-        bitly = Bit.ly(BITLY_ACCESS_TOKEN);
     }
 
     @Override
@@ -35,30 +32,32 @@ public class TorrentTest extends ModuleBase {
         updateMatch(message);
         String messageBody = message.getMessage();
 
-        if (match.equals(SEARCH_REGEX)) {
+        if (match.equals(TORRENT_REGEX) || match.equals(T_REGEX)) {
             Matcher matcher = Pattern.compile(match).matcher(message.getMessage());
 
             if (matcher.find()) {
                 String userQuery = matcher.group(1).replaceAll("\\s+", "+");
-                String messageToSend = X1337_URL + userQuery + POSTFIX
-                            + "\n" + TORRENTZ_URL + userQuery;
-
-                    chatbot.sendMessage(messageToSend);
-                    return true;
-                } else {
-                    chatbot.sendMessage("Coś żeś pojebał");
-                    return true;
-                }
+                String messageToSend = X1337_URL + userQuery + X1337_POSTFIX
+                            + "\n\n" + TORRENTZ_URL + userQuery;
+                chatbot.sendMessage(messageToSend);
+                return true;
+            } else {
+                chatbot.sendMessage("Coś żeś pojebał");
+                return true;
             }
+        }
         return false;
     }
 
     @Override
+    @SuppressWarnings("Duplicates")
     public String getMatch(Message message) {
         String messageBody = message.getMessage();
 
-        if (messageBody.matches(SEARCH_REGEX)) {
-            return SEARCH_REGEX;
+        if (messageBody.matches(TORRENT_REGEX)) {
+            return TORRENT_REGEX;
+        } else if (messageBody.matches(T_REGEX)) {
+            return T_REGEX;
         }
         return "";
     }
@@ -66,7 +65,8 @@ public class TorrentTest extends ModuleBase {
     @Override
     public ArrayList<String> getCommands() {
         ArrayList<String> commands = new ArrayList<>();
-        commands.add(Utils.TO_COMMAND(SEARCH_REGEX));
+        commands.add(Utils.TO_COMMAND(TORRENT_REGEX));
+        commands.add(Utils.TO_COMMAND(T_REGEX));
         return commands;
     }
 }
